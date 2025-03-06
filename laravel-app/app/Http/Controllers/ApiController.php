cb40f4c4e506a4b4fc7c7bc75b8984cd34690030
OliverKochovski <oliver@nteams.com>
2025-03-06 10:09:48 +0100

    Added changes for deployement

--- laravel-app/app/Http/Controllers/ApiController.php
+++ laravel-app/app/Http/Controllers/ApiController.php
@@ -8,7 +8,7 @@ class ApiController extends Controller
 {
     public function status()
     {
-        return response()->json(['status' => 'API is working and was updated for Deployment!']);
+        return response()->json(['status' => 'API is working!']);
     }
 
     public function store()
